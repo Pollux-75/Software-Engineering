f50f31e9919011a9368b3e0d7b97f9328483c689
JACKAL705 <1275370409@qq.com>
2021-09-09 22:06:03 +0800

    <feat>(main):accomplish func get_words

---
+++ 031902415/1st_personal_programming/source_code/Sensitive_words_detection/main.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include "windows.h"
+using namespace std;
+
+/*--------------------------------------------------*/
+struct word
+{
+	bool isEnd = false;
+	unordered_map<string, word> next;
+};
+
+ifstream file_in_words;
+ifstream file_in_org;
+ofstream file_out_ans;
+
+string s_temp;			// 用于读入文件中的每行
+unordered_map<string, word> words_hash_table;
+// words_hash_table表示敏感词表
+
+/*--------------------------------------------------*/
+
+void open_file(char* argv[])	// 打开文件
+{
+	// 打开文件
+	// *argv[1]:敏感词词汇文件
+	// *argv[2]:待检测文件
+	// *argv[3]:答案文件
+
+/*
+	cout << endl << argv[1] << endl;
+	cout << endl << argv[2] << endl;
+	cout << endl << argv[3] << endl;
+*/
+
+/*
+	file_in_words.open(argv[1]);
+	file_in_org.open(argv[2]);
+	file_out_ans.open(argv[3]);		
+*/
+
+	// 直接从sln同个目录下读文件（方便debug），在最终阶段需要改成在控制台读
+	file_in_words.open("words.txt");
+	file_in_org.open("org.txt");
+	file_out_ans.open("ans.txt");
+
+
+	// 文件打开失败，说明并结束程序
+	if (!(file_in_words.is_open() && file_in_org.is_open() && file_out_ans.is_open()))
+	{
+		if (!file_in_words.is_open())	cout << "敏感词词汇文件打开失败" << endl;
+		if (!file_in_org.is_open())		cout << "待检测文件打开失败" << endl;
+		if (!file_out_ans.is_open())	cout << "答案文件打开失败" << endl;
+		exit(0);
+	}
+}
+/*--------------------------------------------------*/
+void close_file()	// 关闭文件
+{
+	// 关闭文件
+	file_in_words.close();
+	file_in_org.close();
+	file_out_ans.close();
+	cout << "程序运行完成";
+}
+/*--------------------------------------------------*/
+
+static string UTF8ToGBK(const char* strUTF8)	// UTF8转GBK
+{
+	int len = MultiByteToWideChar(CP_UTF8, 0, strUTF8, -1, NULL, 0);
+	wchar_t* wszGBK = new wchar_t[len + 1];
+	memset(wszGBK, 0, len * 2 + 2);
+	MultiByteToWideChar(CP_UTF8, 0, strUTF8, -1, wszGBK, len);
+	len = WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, NULL, 0, NULL, NULL);
+	char* szGBK = new char[len + 1];
+	memset(szGBK, 0, len + 1);
+	WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, szGBK, len, NULL, NULL);
+	string strTemp(szGBK);
+	if (wszGBK) delete[] wszGBK;
+	if (szGBK) delete[] szGBK;
+	return strTemp;
+}
+
+/*--------------------------------------------------*/
+
+void get_words()	// 获得敏感词列表
+{
+	while (getline(file_in_words,s_temp))
+	{
+		// 读入一个敏感词
+		int i_temp=0, l_temp;
+		unordered_map<string, word> *word_hash_temp = &words_hash_table;
+		// word_hash_temp是临时的敏感词表节点
+		// words_hash_table是全部敏感词表
+
+		s_temp = UTF8ToGBK(s_temp.data());		// UTF8转GBK
+		l_temp = s_temp.length();				// 获得s_temp长度
+
+		while (i_temp < l_temp)	// 遍历s_temp，每次处理一个汉字/字母
+		{
+			string word_string_temp = "";
+			// 临时存储一个汉字/字母
+
+			if (s_temp[i_temp] & 0x80)	// 发现一个汉字
+			{
+				word_string_temp = word_string_temp + s_temp[i_temp] + s_temp[i_temp + 1];
+				i_temp += 2;
+			}
+			else						//发现一个字母
+			{
+				word_string_temp += s_temp[i_temp];
+				i_temp += 1;
+			}
+
+			if (!(*word_hash_temp).count(word_string_temp))	// 如果当前汉字/字母在表头不存在										// 如果当前汉字/字母在表头
+			{
+				(*word_hash_temp)[word_string_temp] = *(new word);	
+				// 新建该汉字/字母
+			}
+
+			if (i_temp >= l_temp)	// 如果已经是敏感词表最后一个汉字/字母
+			{
+				(*word_hash_temp)[word_string_temp].isEnd = true;
+			}
+			else					// 否则，还有要添加的汉字/字母
+			{
+				word_hash_temp = &(*word_hash_temp)[word_string_temp].next;
+				// 移动到该汉字/字母的位置
+			}
+		}
+	}
+}
+/*--------------------------------------------------*/
+void deal_org()		// 检测敏感词
+{
+
+}
+/*--------------------------------------------------*/
+void write_ans()	// 输出答案
+{
+
+}
+/*--------------------------------------------------*/
+int main(int argc, char *argv[])
+{
+	open_file(argv);	// 打开文件
+
+	get_words();		// 获得敏感词列表
+
+	deal_org();			// 检测敏感词
+
+	write_ans();		// 输出答案
+
+	close_file();		// 关闭文件
+
+	return 0;
+}
